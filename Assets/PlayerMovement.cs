9b4dc5e88c5b6d1996266f7fa0f1a572562ad3e9
peterli98 <pedurli98@gmail.com>
2017-10-19 19:33:58 -0400

    added simple player controlled movements, moving left and right

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody playerBody;
+    public float forwardForce = 1000f;
+    public float sideForce = 1000f;
+    public bool moveLeft = false;
+    public bool moveRight = false;
+
+    void Update()
+    {
+        if(Input.GetKey("a"))
+        {
+            moveLeft = true;
+        }
+
+        if (Input.GetKey("d"))
+        {
+            moveRight = true;
+        }
+    }
+
+    // Updating Physics
+    void FixedUpdate () {
+
+        playerBody.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if(moveLeft == true)
+        {
+            playerBody.AddForce(-sideForce * Time.deltaTime, 0, 0);
+            moveLeft = false;
+        }
+
+        if (moveRight == true)
+        {
+            playerBody.AddForce(sideForce * Time.deltaTime, 0, 0);
+            moveRight = false;
+        }
+    }
+}
